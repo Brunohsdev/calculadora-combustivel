b5b5f8790bea1ae1a960b1c10690b093027e30c2
Brunohsdev <bhas795002@gmail.com>
2025-04-08 12:31:42 +0000

    salvar

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
-let valorGasosa = [];
-function gasosa(){
-    //pergunta a distancia percorrida
+//função que calcula o consumo médio que o usario faz;
+function calculoConsumomedio(){
+    //Deve ser solicitado ao usuário a distância percorrida de sua casa até seu trabalho (em km). Uma caixa de diálogo prompt ou form html
     let distanciaPercorrida = parseFloat(prompt('Qual é a distancia percorrida da sua casa até o trabalho (em km) ? '));
-    //pergunta o consumo médio do veiculo
-    let consumoMedioDoVeiculo = parseFloat(prompt('Qual é o consumo médio do veiculo(em Km/L) ? '));
-    // faz a conta do consumo necessário
-    let consumo_NecessarioLitros= distanciaPercorrida/ consumoMedioDoVeiculo;
-    //operador ternario pra a palavra litros ou litro
-    let palavraLitro = consumoMedioDoVeiculo > 1 ? 'litros' : 'litro';
-    //traz o alert com as variaveis consumoNecessarioLitros e a variavel do operador ternario
-    alert(`O consumo necessário é ${consumo_NecessarioLitros}  ${palavraLitro}`);
-}
-//chama a função sem parametros
-gasosa()
+    //Deve ser solicitado ao usuário o consumo médio do veículo (em Km/L).
+    let consumoMedio = parseFloat(prompt('Qual é o consumo médio do veiculo(em Km/L) ? '));
+    //adicionando uma condicional que verifica se o valor está negativo se sim ele acaba o codigo
+    if(distanciaPercorrida < 0 || consumoMedio <0){
+        alert('==ERROR==');
+        return;
+    }else{
+        let consumo_NecessarioLitros = distanciaPercorrida/ consumoMedio;
+        return consumo_NecessarioLitros;
 
-//abre a funcao para perguntar a questão da quantidades de posto e a media junto com o maior e menor valor 
-function postoEMedia(){
-    // pergunta a quantidade de postos que foram pesquisados
-    quantidadeDePostos = prompt('Em quantos postos você pesquisou? ');
-    //coloca o número de postos em quantidade de pessoas
-    quantidadeDePostos = Number(quantidadeDePostos);
-    //colocoa o laço for 
-    for(let i = 0; i != quantidadeDePostos; i++){
-        let valor = prompt(`Digite o valor encontrado (em R$) no posto ${i+1}`);
-        valorGasosa.push(valor);
     }
-    let menor_Valor_Pesquisado;
-    let maior_Valor_Pesquisado;
-    media = soma_Total_Valores_Pesquisados/ quantidadeDePostos;
-    gasto_Diario = 2*(consumo_NecessarioLitros* menor_Valor_Pesquisado)
-    console.log(valorGasosa);    
+
 }
 
-postoEMedia();
+//função que calcula a media entre os postos e calcula o gasto diario
+function mediaEGastoDiario(){
+    //atribuo uma lista e chamo função calculoConsumomedio
+    let valores = [];
+    let consumo_NecessarioLitros = calculoConsumomedio();
+    let valor_Total = 0;
+    //peço que o user digite em quantos postos ele pesquisou para usar como o atributo do laço for
+    let qtdPostos = parseInt(prompt('Em quantos postos você pesquisou ?'));
+    //laço de repetição que coloca os valores em uma lista e soma todos os valores
+    for(let i = 0; i != qtdPostos; i++){
+        let valor = parseFloat(prompt(`Digite o valor encontrado no Posto ${i+1}`));
+        valores.push(valor)
+        valor_Total += valor; 
+    }
+    //calcula a média, menor valor e o gasto diario
+    let media = Math.round(valor_Total/qtdPostos);
+    let menor_Valor_Pesquisado = Math.min(...valores);
+    gasto_Diario = 2*(consumo_NecessarioLitros * menor_Valor_Pesquisado);
+    
+    return {
+        consumo_NecessarioLitros,
+        media,
+        menor_Valor_Pesquisado,
+        gasto_Diario,
+    }    
+}
+
+//funcao para exibir tela
+function exibirTela(){
+let mediaFunction = mediaEGastoDiario();
+//teste no console
+console.log(`Média ${mediaFunction.media}`);
+console.log(`Menor valor ${mediaFunction.menor_Valor_Pesquisado}`);
+console.log(`gasto diario ${mediaFunction.gasto_Diario}`);
 
 
+alert(`O consumo necessário é de ${mediaFunction.consumo_NecessarioLitros} litros
+O menor valor pesquisado é de ${mediaFunction.menor_Valor_Pesquisado} reais
+A média de valores pesquisados é de R$ ${mediaFunction.media}
+O gasto diário (ida e volta) é R$ ${mediaFunction.gasto_Diario} reais`);
+}
 
+exibirTela();
+//fazendo a segunda parte do desafio
