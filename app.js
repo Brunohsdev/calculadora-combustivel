1b47e5579d2349b5fec66dcc6b5ea2db45162deb
Brunohsdev <bhas795002@gmail.com>
2025-04-11 11:30:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,71 @@
-//função que calcula o consumo médio que o usario faz;
-function calculoConsumomedio(){
-    //Deve ser solicitado ao usuário a distância percorrida de sua casa até seu trabalho (em km). Uma caixa de diálogo prompt ou form html
+///colocar função
+function br(){
+document.write("<br>")
+}
+
+function ExibirTexto(texto, tag){
+    document.write(texto);
+    br();
+}
+
+while(true){    
+//declaração de variaveis  e pedido da distância casa/trabalho
     let distanciaPercorrida = parseFloat(prompt('Qual é a distancia percorrida da sua casa até o trabalho (em km) ? '));
-    //Deve ser solicitado ao usuário o consumo médio do veículo (em Km/L).
-    let consumoMedio = parseFloat(prompt('Qual é o consumo médio do veiculo(em Km/L) ? '));
-    //adicionando uma condicional que verifica se o valor está negativo se sim ele acaba o codigo
-    if(distanciaPercorrida < 0 || consumoMedio <0){
-        alert('==ERROR==');
-        return;
+    let consumoMedio;
+    //testando se o número é válido ou se não é um número
+    if (distanciaPercorrida <= 0 || isNaN(distanciaPercorrida)){
+        alert('===ERROR===\n===Value is invalid===');
     }else{
-        let consumo_NecessarioLitros = distanciaPercorrida/ consumoMedio;
-        return consumo_NecessarioLitros;
-
+     consumoMedio = parseFloat(prompt('Qual é o consumo médio do veiculo(em Km/L) ? '));
+     //testando se o número é válido ou se não é um número
+     if (consumoMedio <= 0 || isNaN(consumoMedio)){
+         alert('===ERROR=== \n===Value is invalid===');
+     }
+     //calculando o consumo necessário do usuario
+     else{
+         var consumo_NecessarioLitros = distanciaPercorrida/ consumoMedio;
+         break;
+ 
+     }
     }
-
 }
 
-//função que calcula a media entre os postos e calcula o gasto diario
-function mediaEGastoDiario(){
-    //atribuo uma lista e chamo função calculoConsumomedio
-    let valores = [];
-    let consumo_NecessarioLitros = calculoConsumomedio();
-    let valor_Total = 0;
-    //peço que o user digite em quantos postos ele pesquisou para usar como o atributo do laço for
-    let qtdPostos = parseInt(prompt('Em quantos postos você pesquisou ?'));
-    //laço de repetição que coloca os valores em uma lista e soma todos os valores
-    for(let i = 0; i != qtdPostos; i++){
-        let valor = parseFloat(prompt(`Digite o valor encontrado no Posto ${i+1}`));
-        valores.push(valor)
-        valor_Total += valor; 
-    }
-    //calcula a média, menor valor e o gasto diario
-    let media = Math.round(valor_Total/qtdPostos);
-    let menor_Valor_Pesquisado = Math.min(...valores);
-    gasto_Diario = 2*(consumo_NecessarioLitros * menor_Valor_Pesquisado);
+
     
-    return {
-        consumo_NecessarioLitros,
-        media,
-        menor_Valor_Pesquisado,
-        gasto_Diario,
-    }    
+    
+    let valorEncontradoNoPosto;
+    let valorTotal = 0;
+    let media;
+    let menorValorPesquisado = 1000;
+    let gastoDiario;
+    let qtdPostos = parseInt(prompt('Em quantos postos você pesquisou ?'));
+    if (qtdPostos <= 0 || isNaN(qtdPostos)){
+        alert('===ERROR=== \n===Value is invalid===');
+    }  else{
+        for(let i = 0; i != qtdPostos; i++){
+            let mensagemDoPosto = `encontrado no posto ${i + 1}`;
+            valorEncontradoNoPosto = parseFloat(prompt(`Digite o valor (em R$) ${mensagemDoPosto}`));
+            if (valorEncontradoNoPosto <= 0 || isNaN(valorEncontradoNoPosto)){
+                alert(`===ERROR=== \n===Value is invalid===\n===Digite novamente o valor ${mensagemDoPosto}===`);
+                i -= 1;
+            }else{
+                valorTotal += valorEncontradoNoPosto;  
+                
+            if(valorEncontradoNoPosto < menorValorPesquisado){
+                menorValorPesquisado = valorEncontradoNoPosto;
+            }
+        }
+    }
+    media = parseFloat(valorTotal)/qtdPostos;
+    gastoDiario = 2*(consumo_NecessarioLitros*menorValorPesquisado);
+    console.log(menorValorPesquisado);
+    console.log(valorTotal);
+    console.log(valorEncontradoNoPosto);
+    console.log(media);
+    ExibirTexto('===Resultado===', 'h2');
+    ExibirTexto(`O consumo necessário é de ${consumo_NecessarioLitros.toFixed(2)}`, 'p');
+    ExibirTexto(`O menor valor foi de ${menorValorPesquisado}`, 'p');
+    ExibirTexto(`A média dos valores pesquisados é de ${media.toFixed(2)}`,'p');
+    ExibirTexto(`o gasto diario (ida e volta) é de ${gastoDiario.toFixed(2)}`,'p');
+    
 }
-
-//funcao para exibir tela
-function exibirTela(){
-let mediaFunction = mediaEGastoDiario();
-//teste no console
-console.log(`Média ${mediaFunction.media}`);
-console.log(`Menor valor ${mediaFunction.menor_Valor_Pesquisado}`);
-console.log(`gasto diario ${mediaFunction.gasto_Diario}`);
-
-
-alert(`O consumo necessário é de ${mediaFunction.consumo_NecessarioLitros} litros
-O menor valor pesquisado é de ${mediaFunction.menor_Valor_Pesquisado} reais
-A média de valores pesquisados é de R$ ${mediaFunction.media}
-O gasto diário (ida e volta) é R$ ${mediaFunction.gasto_Diario} reais`);
-}
-
-exibirTela();
-//fazendo a segunda parte do desafio
